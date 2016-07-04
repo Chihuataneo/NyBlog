ff889cc1458e3dc1937b3df0111123798e657e64
Nyloner <nyloner.root@gmail.com>
2016-07-04 22:54:34 +0800

    update

--- nyloner/settings.py
+++ nyloner/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nyblog',
         'USER':'root',
-        'PASSWORD':'199513future',
+        'PASSWORD':'',
         'HOST':'',
         'PORT':'',
         }
