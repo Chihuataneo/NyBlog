ff0da9ca3c39f0a12210e13717d729d74beb8b34
Nyloner <nyloner.root@gmail.com>
2017-05-07 13:54:20 +0800

    add settings

--- nyloner/settings.py
+++ nyloner/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'nyloner.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nyblog',
-        'USER':'root',
-        'PASSWORD':'199513future',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
         'HOST':'',
         'PORT':'',
         }
@@ -94,7 +94,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -108,13 +108,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# 当运行 python manage.py collectstatic 的时候
-# STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
-# 把这些文件放到一起是为了用apache等部署的时候更方便
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-
-# 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
-# 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "common_static"),
 )
