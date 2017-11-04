56c9357c4e3b36d2f93c751924a5004aec940afa
Nyloner <nyloner.root@gmail.com>
2017-11-04 23:14:38 +0800

    add converter

--- blog/logic/logic_tools.py
+++ blog/logic/logic_tools.py
@@ -1,6 +1,7 @@
 def get_tools():
     tools = [
+        {'url': '/coder', 'title': 'Coder'},
         {'url': '/proxy', 'title': '代理IP'},
-        {'url': '/coder', 'title': 'Coder'}
+        {'url': '/converter', 'title': '文档转换'}
     ]
     return tools
